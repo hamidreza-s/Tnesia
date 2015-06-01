cd38efa4a9994674ad3e4a9e4ae378b6403b43d2
Hamidreza Soleimani <hamidreza.s@gmail.com>
2015-06-01 19:19:24 +0430

    adding common test (ct) boilerplate

---
+++ test/tnesia_api_SUITE.erl
@@ -0,0 +1,64 @@
+-module(tnesia_api_SUITE).
+-include_lib("common_test/include/ct.hrl").
+
+-compile(export_all).
+
+%%====================================================================
+%% CT Callbacks
+%%====================================================================
+
+%%--------------------------------------------------------------------
+%% suite | groups | all
+%%--------------------------------------------------------------------
+
+suite() -> [{timetrap, {seconds, 20}}].
+
+groups() -> [].
+
+all() ->
+    [ {exports, Functions} | _ ] = ?MODULE:module_info(),
+    [ FName || {FName, _} <- lists:filter(
+                               fun ({module_info,_}) -> false;
+                                   ({all,_}) -> false;
+                                   ({init_per_suite,1}) -> false;
+                                   ({end_per_suite,1}) -> false;
+                                   ({_,1}) -> true;
+                                   ({_,_}) -> false
+                               end, Functions)].
+
+%%--------------------------------------------------------------------
+%% init_per_suite | end_per_suite
+%%--------------------------------------------------------------------
+init_per_suite(Config) ->
+    Config.
+
+end_per_suite(_Config) ->
+    ok.
+
+%%--------------------------------------------------------------------
+%% init_per_group | end_per_group
+%%--------------------------------------------------------------------
+init_per_group(_group, Config) ->
+    Config.
+
+end_per_group(_group, Config) ->
+    Config.
+
+%%--------------------------------------------------------------------
+%% init_per_testcase | end_per_testcase
+%%--------------------------------------------------------------------
+init_per_testcase(TestCase, Config) ->
+    Config.
+
+end_per_testcase(TestCase, Config) ->
+    Config.
+
+%%====================================================================
+%% Test Cases
+%%====================================================================
+
+%%--------------------------------------------------------------------
+%% test_tnesia_api
+%%--------------------------------------------------------------------
+test_tnesia_api(Config) ->
+    {skip, "Will be implemented"}.
